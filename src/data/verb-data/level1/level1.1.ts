696cdafbaa51cdb8839955fd363a9fe346ff6363
[dyad] <realdreamcars78@gmail.com>
2025-10-14 06:43:57 -0700

    [dyad] Ajout de un nouveau verbe - wrote 1 file(s)

---
+++ src/data/verb-data/level1/level1.1.ts
@@ -0,0 +1,32 @@
+import { Verb } from '../../verbs';
+
+const manger: Verb = {
+  name: "manger",
+  conjugations: {
+    "indicatif présent": { "je": "mange", "tu": "manges", "il/elle": "mange", "nous": "mangeons", "vous": "mangez", "ils/elles": "mangent" },
+    "indicatif imparfait": { "je": "mangeais", "tu": "mangeais", "il/elle": "mangeait", "nous": "mangeions", "vous": "mangeiez", "ils/elles": "mangeaient" },
+    "indicatif passé simple": { "je": "mangeai", "tu": "mangeas", "il/elle": "mange", "nous": "mangeâmes", "vous": "mangeâtes", "ils/elles": "mangeèrent" },
+    "indicatif futur simple": { "je": "mangerai", "tu": "mangeras", "il/elle": "mangera", "nous": "mangerons", "vous": "mangez", "ils/elles": "mangeront" },
+    "indicatif passé composé": { "je": "ai mangé", "tu": "as mangé", "il/elle": "a mangé", "nous": "avons mangé", "vous": "avez mangé", "ils/elles": "ont mangé" },
+    "indicatif plus-que-parfait": { "je": "avais mangé", "tu": "avais mangé", "il/elle": "avait mangé", "nous": "avions mangé", "vous": "aviez mangé", "ils/elles": "avaient mangé" },
+    "indicatif passé antérieur": { "je": "eus mangé", "tu": "eus mangé", "il/elle": "eut mangé", "nous": "eûmes mangé", "vous": "eûtes mangé", "ils/elles": "eurent mangé" },
+    "indicatif futur antérieur": { "je": "aurai mangé", "tu": "auras mangé", "il/elle": "aura mangé", "nous": "aurons mangé", "vous": "aurez mangé", "ils/elles": "auront mangé" },
+    "subjonctif présent": { "je": "que je mange", "tu": "que tu manges", "il/elle": "qu'il mange", "nous": "que nous mangions", "vous": "que vous mangiez", "ils/elles": "qu'ils mangent" },
+    "subjonctif imparfait": { "je": "que je fusse", "tu": "que tu fusses", "il/elle": "qu'il fût", "nous": "que nous fussions", "vous": "que vous fussiez", "ils/elles": "qu'ils fussent" },
+    "subjonctif passé": { "je": "que j'aie mangé", "tu": "que tu aies mangé", "il/elle": "qu'il ait mangé", "nous": "que nous ayons mangé", "vous": "que vous ayez mangé", "ils/elles": "qu'ils aient mangé" },
+    "subjonctof plus-que-parfait": { "je": "que j'eusse mangé", "tu": "que tu eusses mangé", "il/elle": "qu'il eût mangé", "nous": "que nous eussions mangé", "vous": "que vous eussiez mangé", "ils/elles": "qu'ils eussent mangé" },
+    "conditionnel présent": { "je": "mangeais", "tu": "mangeais", "il/elle": "mangeait", "nous": "mangeions", "vous": "mangeiez", "ils/elles": "mangeaient" },
+    "conditionnel passé première forme": { "je": "aurais mangé", "tu": "aurais mangé", "il/elle": "aurait mangé", "nous": "aurions mangé", "vous": "auriez mangé", "ils/elles": "auraient mangé" },
+    "conditionnel passé deuxième forme": { "je": "eusse mangé", "tu": "eusses mangé", "il/elle": "eût mangé", "nous": "eussions mangé", "vous": "eussiez mangé", "ils/elles": "eussent mangé" },
+    "impératif présent": { "tu": "mange", "nous": "mangeons", "vous": "mangez" },
+    "impératif passé": { "tu": "aie mangé", "nous": "ayons mangé", "vous": "ayez mangé" },
+    "infinitif présent": "manger",
+    "infinitif passé": "avoir mangé",
+    "participe présent": "mangent",
+    "participe passé": "mangé (ayant mangé)",
+    "gérondif présent": "en mangeant",
+    "gérondif passé": "en ayant mangé",
+  },
+};
+
+export default manger;

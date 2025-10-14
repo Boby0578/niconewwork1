17b447cc08f43e838a8fd6e8a20dedf405e4586c
[dyad] <realdreamcars78@gmail.com>
2025-10-13 17:54:08 -0700

    [dyad] Mise à jour de l'icône du microphone - wrote 1 file(s)

--- src/components/icons/NewMicrophoneIcon.tsx
+++ src/components/icons/NewMicrophoneIcon.tsx
@@ -12,7 +12,11 @@ const NewMicrophoneIcon: React.FC<NewMicrophoneIconProps> = ({ className }) => {
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.49 6-3.31 6-6.72h-1.7z" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M12 15C14.21 15 16 13.21 16 11V6C16 3.79 14.21 2 12 2C9.79 2 8 3.79 8 6V11C8 13.21 9.79 15 12 15ZM11.5 15V19H12.5V15H11.5ZM9 19V20H15V19H9Z"
+      />
     </svg>
   );
 };
